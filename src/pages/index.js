5eba58c500bcbdbef0cc0778144ba3f376af4914
tankxu <hellotank@qq.com>
2021-10-13 19:24:20 +0800

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StaticImage } from "gatsby-plugin-image"
+import { StaticImage } from "gatsby-plugin-image";
 
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
@@ -20,222 +20,83 @@ const navigation = [
   { name: "Monkit设计组件", href: "/monkit" },
   { name: "设计博客", href: "/blog" },
 ];
-const features = [
-  {
-    name: "Push to Deploy",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi vitae lobortis.",
-    icon: CloudUploadIcon,
-  },
-  {
-    name: "SSL Certificates",
-    description:
-      "Qui aut temporibus nesciunt vitae dicta repellat sit dolores pariatur. Temporibus qui illum aut.",
-    icon: LockClosedIcon,
-  },
-  {
-    name: "Simple Queues",
-    description:
-      "Rerum quas incidunt deleniti quaerat suscipit mollitia. Amet repellendus ut odit dolores qui.",
-    icon: RefreshIcon,
-  },
-  {
-    name: "Advanced Security",
-    description:
-      "Ullam laboriosam est voluptatem maxime ut mollitia commodi. Et dignissimos suscipit perspiciatis.",
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: "Powerful API",
-    description:
-      "Ab a facere voluptatem in quia corrupti veritatis aliquam. Veritatis labore quaerat ipsum quaerat id.",
-    icon: CogIcon,
-  },
-  {
-    name: "Database Backups",
-    description:
-      "Quia qui et est officia cupiditate qui consectetur. Ratione similique et impedit ea ipsum et.",
-    icon: ServerIcon,
-  },
-];
-const blogPosts = [
-  {
-    id: 1,
-    title: "Boost your conversion rate",
-    href: "#",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { name: "Article", href: "#" },
-    imageUrl:
-      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-    preview:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
-    author: {
-      name: "Roel Aufderehar",
-      imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      href: "#",
-    },
-    readingLength: "6 min",
-  },
-  {
-    id: 2,
-    title: "How to use search engine optimization to drive sales",
-    href: "#",
-    date: "Mar 10, 2020",
-    datetime: "2020-03-10",
-    category: { name: "Video", href: "#" },
-    imageUrl:
-      "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-    preview:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
-    author: {
-      name: "Brenna Goyette",
-      imageUrl:
-        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      href: "#",
-    },
-    readingLength: "4 min",
-  },
-  {
-    id: 3,
-    title: "Improve your customer experience",
-    href: "#",
-    date: "Feb 12, 2020",
-    datetime: "2020-02-12",
-    category: { name: "Case Study", href: "#" },
-    imageUrl:
-      "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
-    preview:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
-    author: {
-      name: "Daniela Metz",
-      imageUrl:
-        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      href: "#",
-    },
-    readingLength: "11 min",
-  },
-];
-const footerNavigation = {
-  solutions: [
-    { name: "Marketing", href: "#" },
-    { name: "Analytics", href: "#" },
-    { name: "Commerce", href: "#" },
-    { name: "Insights", href: "#" },
-  ],
-  support: [
-    { name: "Pricing", href: "#" },
-    { name: "Documentation", href: "#" },
-    { name: "Guides", href: "#" },
-    { name: "API Status", href: "#" },
-  ],
-  company: [
-    { name: "About", href: "#" },
-    { name: "Blog", href: "#" },
-    { name: "Jobs", href: "#" },
-    { name: "Press", href: "#" },
-    { name: "Partners", href: "#" },
-  ],
-  legal: [
-    { name: "Claim", href: "#" },
-    { name: "Privacy", href: "#" },
-    { name: "Terms", href: "#" },
-  ],
-  social: [
-    {
-      name: "Facebook",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Instagram",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Twitter",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-        </svg>
-      ),
-    },
-    {
-      name: "GitHub",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Dribbble",
-      href: "#",
-      icon: (props) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-  ],
-};
 
 const ideology = [
   {
     name: "Pure 纯净",
-    descCN: "面向企业的产品体验尤其需要简洁性，每个用户都应当感受到简单清晰的流程和协同工作的便利性，这需要从品牌到界面的整体规划和重视，也需要每个参与产品设计的成员对“纯净”理念的深入理解和持续坚持。",
-    descEN: 'Enterprise-oriented product experience especially needs simplicity. Every user should feel the simple and clear process and the convenience of collaborative work. This requires overall planning and attention from the brand to the interface, as well as the insistence of every member involved in product design. Deep understanding and continuous adherence to the concept of "Pure".',
+    descCN:
+      "面向企业的产品体验尤其需要简洁性，每个用户都应当感受到简单清晰的流程和协同工作的便利性，这需要从品牌到界面的整体规划和重视，也需要每个参与产品设计的成员对“纯净”理念的深入理解和持续坚持。",
+    descEN:
+      'Enterprise-oriented product experience especially needs simplicity. Every user should feel the simple and clear process and the convenience of collaborative work. This requires overall planning and attention from the brand to the interface, as well as the insistence of every member involved in product design. Deep understanding and continuous adherence to the concept of "Pure".',
     image: "../images/pure.png",
     imageAlt: "Pure Symbol",
   },
   {
     name: "Dynamic 动态",
-    descCN: "面对不同客户的多样化需求，以及同一产品在不同环境下的体验差异，产品和体验设计应当动态化适应绝大多数场景，这需要充分关注用户即刻产生的体验反馈，体验或服务框架的扩展性，以及不同场景切换时的体验一致性，才能让功能服务更加强大，印象更加统一，产品体验更加健康。",
-    descEN: "Facing the diversified needs of different customers and the experience differences of the same product in different environments, product and experience design should be dynamically adapted to most scenarios, which requires full attention to the user’s immediate experience feedback and the extension of the experience or service framework In order to make the functional services more powerful, the impressions are more unified, and the product experience is healthier.",
+    descCN:
+      "面对不同客户的多样化需求，以及同一产品在不同环境下的体验差异，产品和体验设计应当动态化适应绝大多数场景，这需要充分关注用户即刻产生的体验反馈，体验或服务框架的扩展性，以及不同场景切换时的体验一致性，才能让功能服务更加强大，印象更加统一，产品体验更加健康。",
+    descEN:
+      "Facing the diversified needs of different customers and the experience differences of the same product in different environments, product and experience design should be dynamically adapted to most scenarios, which requires full attention to the user’s immediate experience feedback and the extension of the experience or service framework In order to make the functional services more powerful, the impressions are more unified, and the product experience is healthier.",
     image: "../images/pure.png",
     imageAlt: "Pure Symbol",
   },
   {
     name: "Thinking 思考",
-    descCN: "以客户为中心的企业级产品体验设计，应当首先考虑客户实际所处的体验环境，包括且不限于企业结构，企业文化，预算成本，售后服务，以及固有的体验资产，品牌印象等，这些因素要求CODING产品服务需要更加灵活，更加具备客户理解力，能够主动为客户思考，增加产品和服务的体验“引力”。",
-    descEN: 'Customer-centric enterprise-level product experience design should first consider the actual experience environment the customer is in, including but not limited to corporate structure, corporate culture, budget cost, after-sales service, and inherent experience assets, brand impressions, etc., these factors Coding products and services are required to be more flexible, have more customer understanding, be able to actively think for customers, and increase the experience "gravity" of products and services.',
+    descCN:
+      "以客户为中心的企业级产品体验设计，应当首先考虑客户实际所处的体验环境，包括且不限于企业结构，企业文化，预算成本，售后服务，以及固有的体验资产，品牌印象等，这些因素要求CODING产品服务需要更加灵活，更加具备客户理解力，能够主动为客户思考，增加产品和服务的体验“引力”。",
+    descEN:
+      'Customer-centric enterprise-level product experience design should first consider the actual experience environment the customer is in, including but not limited to corporate structure, corporate culture, budget cost, after-sales service, and inherent experience assets, brand impressions, etc., these factors Coding products and services are required to be more flexible, have more customer understanding, be able to actively think for customers, and increase the experience "gravity" of products and services.',
     image: "../images/pure.png",
     imageAlt: "Pure Symbol",
   },
+];
+
+const sectionTitleContent = [
+  {
+    titleCN: "理念",
+    titleEN: "IEDOLOGY",
+  },
+  {
+    titleCN: "产品",
+    titleEN: "PRODUCT",
+  },
+  {
+    titleCN: "品牌识别",
+    titleEN: "BRANDING",
+  },
+  {
+    titleCN: "IP形象",
+    titleEN: "Intellectual Property",
+  },
+  {
+    titleCN: "品牌识别",
+    titleEN: "BRANDING",
+  },
+  {
+    titleCN: "色彩",
+    titleEN: "COLOR",
+  },
 ]
+
+// Components
+
+const SectionTitle = ({section}) => {
+  return (
+    <div className="sticky top-0 mx-auto max-w-screen-2xl px-4 text-center lg:text-left ">
+      <div className="flex flex-col w-full lg:w-auto lg:flex-row absolute mt-4">
+        <h2 className="font-semibold text-4xl lg:text-3xl lg:w-1 lg:order-last">{section.titleCN}</h2>
+        <p className="opacity-10 mt-2 text-3xl lg:text-3xl lg:mt-0 lg:writing-mode-tb">{section.titleEN}</p>
+      </div>
+    </div>
+  )
+}
 
 // markup
 const IndexPage = () => {
+  
   return (
     <div className="bg-white">
-      <div className="relative overflow-hidden">
+      <div className="relative">
         <Popover as="header" className="relative">
           <div className="bg-gray-900 pt-6">
             <nav
@@ -350,7 +211,9 @@ const IndexPage = () => {
             </Popover.Panel>
           </Transition>
         </Popover>
+
         <main>
+          {/* Hero */}
           <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
             <div className="mx-auto max-w-7xl lg:px-8">
               <div className="lg:grid lg:grid-cols-2 lg:gap-8">
@@ -419,7 +282,7 @@ const IndexPage = () => {
             </div>
           </div>
 
-          {/* Feature section with screenshot */}
+          {/* 理念 Section */}
           {/* <div className="max-h-screen overflow-y-scroll scroll-snap-y scroll-snap-mandatory">
           <section class="w-full h-screen bg-red-200 scroll-snap-start">
             <div className="mx-auto max-w-7xl lg:px-8">
@@ -433,23 +296,26 @@ const IndexPage = () => {
             Section 3
           </section>
         </div> */}
-          <div className="pt-10 bg-white sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="bg-white pt-14">
+            <SectionTitle section={sectionTitleContent[0]} />
+            {/* {sectionTitle({})} */}
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:pl-32">
               {ideology.map((item) => (
-                    <div className="lg:flex justify-between">
-                      <div className="max-w-3xl mr-8">
-                        <h3 className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">{item.name}</h3>
-                        <p>{item.descCN}</p>
-                        <p>{item.descEN}</p>
-                      </div>
-                      <div className="w-72 grid place-items-center">
-                        <div className="bg-gray-400 w-60 h-32"></div>
-                      </div>
-                    </div>
-                  ))}
+                <div className="lg:flex justify-between mt-32 mb-48">
+                  <div className="max-w-3xl mr-10 text-base">
+                    <h3 className="mb-14 text-2xl font-semibold sm:text-6xl">
+                      {item.name}
+                    </h3>
+                    <p className="leading-relaxed">{item.descCN}</p>
+                    <p className="mt-8">{item.descEN}</p>
+                  </div>
+                  <div className="w-full grid place-items-center">
+                    <div className="bg-gray-400 w-60 h-32"></div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-
         </main>
       </div>
     </div>
